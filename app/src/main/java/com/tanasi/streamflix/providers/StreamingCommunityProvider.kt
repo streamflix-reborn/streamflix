1a1bfbc706f099d9af97884bbeb39da92c64cae1
Or-Cr-DiDio <oraziocristiandidio@gmail.com>
2025-09-08 22:09:28 +0200

    aggiornata url base e User-Agent

--- app/src/main/java/com/tanasi/streamflix/providers/StreamingCommunityProvider.kt
+++ app/src/main/java/com/tanasi/streamflix/providers/StreamingCommunityProvider.kt
@@ -495,7 +495,7 @@ object StreamingCommunityProvider : Provider {
     private interface StreamingCommunityService {
 
         companion object {
-            private const val USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
+            private const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
             fun build(baseUrl: String): StreamingCommunityService {
                 val logging = HttpLoggingInterceptor()
